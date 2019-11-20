c8af429627987b0cafc77295f8618fe7930c9882
GitVide <1162065497@qq.com>
2019-11-20 11:52:09 +0800

    modified

--- newcode/src/tencent2018/greedyQ/Main.java
+++ newcode/src/tencent2018/greedyQ/Main.java
@@ -1,41 +1,31 @@
 package tencent2018.greedyQ;
 
 import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
 
+public class Main {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int N = scanner.nextInt();
         int M = scanner.nextInt();
-        if(N == 0 || M == 0){
-            System.out.println(0);
-            return;
-        }
-        for(int i = M ; i > 0 ; i--){
-            if(isOk(i,N,M)){
-                System.out.println(i);
+        int high = M;
+        int low = 1;
+        int middle;
+        while (low != high) {
+            middle = (low + high + 1) / 2;
+            int sum = 0;
+            int current = middle;
+            for (int i = 0; i < N - 1 ; i++) {
+                sum += current;
+                current = (current + 1) / 2;
+            }
+            if (sum > M) {
+                high = middle - 1;
+            } else if (sum < M) {
+                low = middle;
+            } else if (sum == M) {
+                System.out.println(middle);
                 break;
             }
         }
-
-    }
-    public static boolean isOk(int first,int N,int M){
-        int sum = first;
-        int previous = first;
-        for(int i = 1 ; i < N ; i++){
-            int current;
-            if(previous % 2 == 0){
-                current = previous/2;
-            }else{
-                current = previous/2 + 1;
-            }
-            previous = current;
-            sum += current;
-            if(sum > M){
-                return false;
-            }
-        }
-        return true;
     }
 }

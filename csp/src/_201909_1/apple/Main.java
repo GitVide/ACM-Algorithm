a54d6d367b79f8e15231ea78e38b2cc372cc60dc
GitVide <1162065497@qq.com>
2019-11-27 16:23:36 +0800

    add apple2

--- csp/src/_201909_1/apple/Main.java
+++ csp/src/_201909_1/apple/Main.java
@@ -1,67 +1,35 @@
 package _201909_1.apple;
 
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
-    static class Pair {
-        int w;
-        int index;
-        int d;
-
-        public Pair(int w, int index) {
-            this.w = w;
-            this.index = index;
-            d = 0;
-        }
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int K = scanner.nextInt();
-        int[] E = new int[10];
-        for (int i = 0; i < 10; i++) {
-            E[i] = scanner.nextInt();
-        }
-        Pair[] wArray = new Pair[n];
-        for (int i = 0; i < n; i++) {
-            int wi = scanner.nextInt();
-            wArray[i] = new Pair(wi, i + 1);
-        }
-        Comparator<Pair> cmp = new Comparator<Pair>() {
-            @Override
-            public int compare(Pair o1, Pair o2) {
-                if (o1.w == o2.w) {
-                    if (o1.index < o2.index)
-                        return -1;
-                } else if (o1.w > o2.w) {
-                    return -1;
-                } else {
-                    return 1;
-                }
-                return 0;
+
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        int[][] array = new int[N][M + 1];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M + 1; j++) {
+                array[i][j] = scanner.nextInt();
             }
-        };
-        Arrays.sort(wArray, cmp);
-        for (Pair e:wArray) {
-            System.out.println("权重: "+e.w+" 编号："+e.index+" 序号: "+e.d);
         }
-
-        for (int i = 0; i < n; i++) {
-            wArray[i].d = i % 10;
-            wArray[i].w += E[wArray[i].d];
+        int[] after = new int[N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M + 1; j++) {
+                after[i] += array[i][j];
+            }
         }
-        for (Pair e:wArray) {
-            System.out.println("权重: "+e.w+" 编号："+e.index+" 序号: "+e.d);
+        int T = 0;
+        int k = 0, p = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            T += after[i];
+            int temp = array[i][0] - after[i];
+            if (temp > p) {
+                p = temp;
+                k = i + 1;
+            }
         }
-
-        Arrays.sort(wArray, cmp);
-
-        for (int i = 0; i < K - 1; i++) {
-            System.out.print(wArray[i].index + " ");
-        }
-        System.out.print(wArray[K - 1].index);
+        System.out.println(T + " " + k + " " + p);
     }
 }
